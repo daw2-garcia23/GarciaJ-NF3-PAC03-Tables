1a82aaccd677a5e2a888fbb4a849e0d588556009
daw2-garcia23 <113383229+daw2-garcia23@users.noreply.github.com>
2022-10-20 18:03:42 +0200

    Add files via upload

---
+++ pr1.php
@@ -0,0 +1,35 @@
+<?php
+$db = mysqli_connect('localhost', 'root', '') or 
+    die ('Unable to connect. Check your connection parameters.');
+mysqli_select_db($db,'moviesite') or die(mysqli_error($db));
+
+//create the reviews table
+$query = 'CREATE TABLE review (
+        review_movie_id INTEGER UNSIGNED NOT NULL, 
+        review_date     DATE             NOT NULL, 
+        reviewer_name   VARCHAR(255)     NOT NULL, 
+        review_comment  VARCHAR(255)     NOT NULL, 
+        review_rating   TINYINT UNSIGNED NOT NULL  DEFAULT 0, 
+
+        KEY (review_movie_id)
+    )
+    ENGINE=MyISAM';
+mysqli_query($db,$query) or die(mysqli_error($db));
+
+//insert new data into the reviews table
+$query = <<<ENDSQL
+INSERT INTO review
+    (review_movie_id, review_date, reviewer_name, review_comment,
+        review_rating)
+VALUES 
+    (4, "2008-09-28", "Peppermint Patty", "I wish I'd have seen it
+        sooner!", 3),
+    (3, "2008-09-23", "Marvin Martian", "This is my favorite movie. I
+        didn't wear my flair to the movie but I loved it anyway.", 6),
+    (2, "2008-09-23", "George B.", "I liked this movie, even though I
+        Thought it was an informational video from my travel agent.", 9)
+ENDSQL;
+mysqli_query($db,$query) or die(mysqli_error($db));
+
+echo 'Movie database successfully updated!';
+?>
